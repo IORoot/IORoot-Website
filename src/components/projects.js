81d34ad42aa14d2d408b2b8011a0635e5556a483
Andy Pearson <andy@londonparkour.com>
2022-06-26 09:09:25 +0100

    plugin

--- src/components/projects.js
+++ src/components/projects.js
@@ -23,6 +23,7 @@ const Projects = () => {
                 slug
                 date(formatString: "MMMM D, YYYY")
                 icon
+                desc
                 }
             }
             }
